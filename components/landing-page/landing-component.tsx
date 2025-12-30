e1ae4ab76da75a9d860bb4c15cf218ba79b8cbf7
ZeroQual <pochta@gmail.com>
2025-12-30 17:33:57 +0300

    add landing styling

--- components/landing-page/landing-component.tsx
+++ components/landing-page/landing-component.tsx
@@ -5,8 +5,15 @@
 
 export default async function LandingComponent() {
     return (
-        <section className="flex flex-1 bg-gray-200">
-            Landing Component
+        <section className="flex flex-1 bg-gray-200 items-center justify-center">
+            <div className="text-xl sm:text-2xl/10 font-bold text-blue-400 md:text-3xl/15 lg:text-4xl/20 height-xl">
+                Free And{" "}
+                <span className="bg-linear-to-br from-pink-300 to-blue-600 bg-clip-text text-transparent">
+                    100%
+                </span>{" "}
+                Secure
+                <p className="text-center text-black/50 text-sm sm:text-lg md:text-xl lg:text-2xl">Social Network</p>
+            </div>
         </section>
     )
 }
